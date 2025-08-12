39bd564dd446d30002cb9d5d27b33d4d51daa11a
WuShF <1481542813@qq.com>
2025-08-13 01:00:16 +0800

    feat:强化测试

--- tests/am-tests/include/amtest.h
+++ tests/am-tests/include/amtest.h
@@ -2,23 +2,32 @@
 #define __AMUNIT_H__
 
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 
-#define IOE ({ ioe_init();  })
-#define CTE(h) ({ Context *h(Event, Context *); cte_init(h); })
-#define VME(f1, f2) ({ void *f1(int); void f2(void *); vme_init(f1, f2); })
+#define IOE ({ ioe_init(); })
+#define CTE(h)                                                                 \
+  ({                                                                           \
+    Context *h(Event, Context *);                                              \
+    cte_init(h);                                                               \
+  })
+#define VME(f1, f2)                                                            \
+  ({                                                                           \
+    void *f1(int);                                                             \
+    void f2(void *);                                                           \
+    vme_init(f1, f2);                                                          \
+  })
 #define MPE ({ mpe_init(entry); })
 
 extern void (*entry)();
 
-#define CASE(id, entry_, ...) \
-  case id: { \
-    void entry_(); \
-    entry = entry_; \
-    __VA_ARGS__; \
-    entry_(); \
-    break; \
+#define CASE(id, entry_, ...)                                                  \
+  case id: {                                                                   \
+    void entry_();                                                             \
+    entry = entry_;                                                            \
+    __VA_ARGS__;                                                               \
+    entry_();                                                                  \
+    break;                                                                     \
   }
 
 #endif
