e024bd69356e0ea2c7ccd14c52e08599dcbc508e
WuShF <1481542813@qq.com>
2026-06-09 20:45:55 +0800

    feat: 支持XPI的测试

--- tests/cpu-tests/tests/flash.c
+++ tests/cpu-tests/tests/flash.c
@@ -1,9 +1,11 @@
 #include "trap.h"
 
 uint32_t flash_read(uint32_t addr);
+volatile uint32_t *xpiBase = (volatile uint32_t *)0x30000000;
 int main() {
-  for (int i = 0; i < (1 << 24); i++) {
-    check(flash_read(i) == i);
+  for (int i = 0; i < (1 << 24); i += 4) {
+    printf("%d\t%d\t%d\n", i, flash_read(i), *(xpiBase + i / 4));
+    check(flash_read(i) == i && i == *(xpiBase + i / 4));
   }
   return 0;
 }
